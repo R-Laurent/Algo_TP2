4a5fc7090ce068c466e6c6c0aed1fc2ca619d6a7
l18019951 <123456BMx!>
2021-10-28 15:45:07 +0200

    28/10

--- Algo_TP2/main.java
+++ Algo_TP2/main.java
@@ -21,21 +21,38 @@ public class main {
         words.add(w1);
         words.add(w2);
         HashMap<String,ArrayList<String>> trigrams = new HashMap<>();
+        ArrayList<ArrayList<String>> tri1 = new ArrayList<>();
         for (int i=0;i<words.size();i++){
             String word = words.get(i);
+            ArrayList<String> tri = new ArrayList<String>();
+            //ArrayList<String> liste = new ArrayList<String>();
             for (int j=0;j<word.length()-2;j++){
-                ArrayList<String> tri = new ArrayList<String>();
                 ArrayList<String> liste = new ArrayList<String>();
                 String tr = word.substring(j,j+3);
-                trigrams.put(tr,liste);
                 tri.add(tr);
-                for (int k=0; k< tri.size();k++){ trigrams.get(tri.get(k)).add(word); }
+                trigrams.put(tr,liste);
+                //trigrams.get(tr).add(word);
             }
+            //for (int k=0;k<tri.size();k++){trigrams.get(tri.get(k)).add(word);}
         }
-        System.out.println(trigrams.keySet());
-        for (int i = 0; i<trigrams.get("bon").size();i++){
-            System.out.println("la taille : " + trigrams.get("bon").size());
-            System.out.println(trigrams.get("bon").get(i));
+        for (int i=0;i<words.size();i++){
+            ArrayList<String> tri = new ArrayList<String>();
+            String word = words.get(i);
+            for (int j=0;j<word.length()-2;j++){
+                ArrayList<String> liste = new ArrayList<String>();
+                String tr = word.substring(j,j+3);
+                tri.add(tr);
+            }
+            for (int k=0;k<tri.size();k++){trigrams.get(tri.get(k)).add(word);}
         }
+
+
+
+        System.out.println("clefs : " + trigrams.keySet());
+        System.out.println("values : " + trigrams.values());
+        System.out.println("test : " + trigrams.get("<bo"));
+        trigrams.get("<bo").add("<bonjour>");
+        System.out.println("test2 : " + trigrams.get("<bo"));
+
     }
 }
