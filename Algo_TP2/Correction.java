4ae680173b1b0586e41f666d4b30c1612e636606
l18019951 <123456BMx!>
2021-11-06 15:45:50 +0100

    6/11

--- Algo_TP2/Correction.java
+++ Algo_TP2/Correction.java
@@ -3,29 +3,28 @@ import java.util.*;
 public class Correction {
     Trigrams t;
     ArrayList<ArrayList<String>> corrections = new ArrayList<>();
-    public Correction(String word, Trigrams t,ArrayList<String> fautes){
+    public Correction(Trigrams t,ArrayList<String> fautes){
         for (int i=0;i<fautes.size();i++){corrections.add(correction1Mot(fautes.get(i),t));}
     }
 
+
     public ArrayList<String> correction1Mot(String word, Trigrams t){
-        //System.out.println(word);
         ArrayList<String> correction  = new ArrayList<>();
         HashMap<String,Integer> occurences = new HashMap<String, Integer>();
-        HashSet<String> c = new HashSet<>();
+        //HashSet<String> c = new HashSet<>();
         ArrayList<String> tri = getTrigrams(word);
-        c.add(word);
+        //c.add(word);
         for (int i=0;i<tri.size();i++){
             if (t.trigrams.get(tri.get(i))==null){i++;}
             else {
                 ArrayList<String> mots = new ArrayList<>(t.trigrams.get(tri.get(i)));
                 for (int j = 0; j < mots.size(); j++) {
                     String motCommun = mots.get(j);
-                    c.add(motCommun);
+          //          c.add(motCommun);
                     if (t.trigrams.get(tri.get(i)).contains(motCommun) == true && occurences.containsKey(motCommun) == true) {
                         occurences.computeIfPresent(motCommun,(k,v)-> v+1);
                     }
                     else {Integer compteur = 1; occurences.put(motCommun, compteur);}
-
                 }
             }
         }
@@ -54,28 +53,6 @@ public class Correction {
         for (int i=0;i<5;i++){correction.add(lesDistances.get(i).getKey());}
         return correction;
     }
-
-    public List<Map.Entry<String,Integer>> nbrOccurence(HashSet<String> commun, ArrayList<String> tri){
-        HashMap<String,Integer> occurences = new HashMap<String, Integer>();
-        ArrayList<String> c = new ArrayList<>(commun);
-        for (int i=1;i<c.size();i++){ // iverser clef/valeur et chercher sur le net comment trié map par valeurs
-            String mot = c.get(i);
-            Integer compteur = 0;
-            for (int j=0;j<tri.size();j++){
-                if (t.trigrams.get(tri.get(j)).contains(mot)){compteur++;}
-            }
-            occurences.put(mot,compteur);
-        }
-        List<Map.Entry<String,Integer>> list = new LinkedList<Map.Entry<String, Integer>>(occurences.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-            @Override
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return o2.getValue().compareTo(o1.getValue());
-            }
-        });
-        return list;
-    }
-
     public ArrayList<String> getTrigrams(String word){
         ArrayList<String> tri = new ArrayList<>();
         for (int j=0;j<word.length()-2;j++){
